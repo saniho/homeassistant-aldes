da2f5500c1aa9895ae13bcf70642bb754b5441df
saniho <sanihogithub@gmail.com>
2025-11-03 21:48:12 +0100

    Add unit test + general test

--- custom_components/aldes/entity.py
+++ custom_components/aldes/entity.py
@@ -1,6 +1,6 @@
 """AldesEntity class"""
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-
+from homeassistant.util import dt as dt_util
 
 class AldesEntity(CoordinatorEntity):
     """Aldes entity"""
@@ -13,3 +13,15 @@ class AldesEntity(CoordinatorEntity):
         self.product_serial_number = product_serial_number
         self.reference = reference
         self.modem = modem
+
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        attributes = super().extra_state_attributes or {}
+        for product in self.coordinator.data:
+            if product.get("modem") == self.modem:
+                if "lastUpdatedDate" in product:
+                    last_updated_str = product["lastUpdatedDate"].replace("Z", "+00:00")
+                    attributes["last_api_update"] = dt_util.parse_datetime(last_updated_str)
+                break
+        return attributes
